3a968062b2b69d690bcb2f6aabae99d902b8b52f
SATYAVEER <39643146+veermit@users.noreply.github.com>
2018-08-03 17:49:19 +0530

    SpeedMeasure

---
+++ SpeedGate.ino
@@ -0,0 +1,33 @@
+
+void setup() {
+  
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  bool bPinVal = false;
+
+  unsigned long ulStartTime = 0;
+  unsigned long ulStopTime = 0;
+
+  bPinVal = digitalRead(2);
+
+  if(!bPinVal)
+  {
+    ulStartTime = micros();
+    ulStopTime = 0;
+
+    while(!bPinVal)
+    { 
+      ulStopTime = micros();
+      bPinVal = digitalRead(2);
+    }
+
+    if(ulStopTime > ulStartTime)
+    {
+      Serial.print(ulStopTime - ulStartTime);
+      Serial.println(" us");
+    }
+  }
+}
